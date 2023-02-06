c42ad24e118e8b75ca2d89baa0e2422baccc4543
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-02-06 10:08:25 +0100

    oneDONE

---
+++ Assets/TWO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TWO : MonoBehaviour
+{
+
+    void Start()
+    {
+        for(int i = 100; i >= 0; i-=2)
+        {
+            Debug.Log(i);
+        }    
+    }
+
+    void Update()
+    {
+        
+    }
+}

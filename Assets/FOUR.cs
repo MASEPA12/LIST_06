aa703ba83f519a5fdf795750d38a0e63f7c07f5e
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-02-06 10:36:55 +0100

    ??

--- Assets/FOUR.cs
+++ Assets/FOUR.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class FOUR : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int start;
+    public int end;
+
     void Start()
     {
-        
-    }
+        while (start >= end)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (start % 5 == 0)
+            {
+                Debug.Log(start);
+            }
+            start += 5;
+
+        }
     }
 }

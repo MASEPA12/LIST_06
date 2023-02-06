1b4df7355912dbd00ea20106e1b524b7912b43e8
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2023-02-06 10:09:35 +0100

    CORRECCIO

--- Assets/TWO.cs
+++ Assets/TWO.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class TWO : MonoBehaviour
 {
-
     void Start()
     {
         for(int i = 100; i >= 0; i-=2)
         {
             Debug.Log(i);
         }    
-    }
-
-    void Update()
-    {
-        
     }
 }
